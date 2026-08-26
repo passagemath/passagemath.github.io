ce4c77c1389dfe16996fd5af0a60bc039c7f2fe2
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2026-08-25 17:21:02 -0700

    docs/10.8/: Update from /Users/mkoeppe/s/sage/sage-rebasing/wheels/passagemath_doc_html-10.8.10-py3-none-any.whl

--- docs/10.8/html/es/tutorial/_static/documentation_options.js
+++ docs/10.8/html/es/tutorial/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '10.8.9',
+    VERSION: '10.8.10',
     LANGUAGE: 'es',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/10.8/html/pt/a_tour_of_sage/_static/documentation_options.js
+++ docs/10.8/html/pt/a_tour_of_sage/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '10.8.9',
+    VERSION: '10.8.10',
     LANGUAGE: 'pt-BR',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
